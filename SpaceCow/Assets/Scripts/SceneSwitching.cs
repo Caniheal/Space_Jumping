0cb6137fb4f4676713444ddfbdd3f877593c0f71
Caniheal <37191567+Caniheal@users.noreply.github.com>
2020-02-18 15:06:15 -0500

    Script for changing scenes

---
+++ SpaceCow/Assets/Scripts/SceneSwitching.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitching : MonoBehaviour
+{
+    private void Start()
+    {
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void SettingsMenu()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits Menu");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
+    }
+}
